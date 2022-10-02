8fa7e803b14927b3dbed1c00482a15f4db9021af
Samy A <sam_dd@hotmail.fr>
2022-10-02 21:01:04 +0100

    Creation of an other test project for testing the integration of the APIs

---
+++ WeatherForecast.Cli.Tests.Integration/Clients/MusementApiClientTests.cs
@@ -0,0 +1,25 @@
+﻿using WeatherForecast.Cli.Clients;
+using WeatherForecast.Cli.Models;
+
+namespace WeatherForecast.Cli.Tests.Integration.Clients;
+public class MusementApiClientTests
+{
+    [Fact]
+    public async Task GetCitiesAsyncTest()
+    {
+        // Arrange
+        HttpClient httpClient = new()
+        {
+            BaseAddress = new Uri("https://sandbox.musement.com")
+        };
+
+        MusementApiClient musementApiClient = new(httpClient);
+
+        // Act
+        ICollection<City>? cities = await musementApiClient.GetCitiesAsync();
+
+        // Assert
+        cities.Should().NotBeNull();
+        cities.Should().NotBeEmpty();
+    }
+}
