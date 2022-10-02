5a76d6e8587790f9cb17ab431d5bb5f1672465df
Samy A <sam_dd@hotmail.fr>
2022-10-02 15:26:10 +0100

    First Implementation of  the IMusementApiClient interface

---
+++ WeatherForecast.Cli.Tests/Clients/MusementApiClientTests.cs
@@ -0,0 +1,34 @@
+﻿using System.Net;
+using WeatherForecast.Cli.Clients;
+using WeatherForecast.Cli.Models;
+
+namespace WeatherForecast.Cli.Tests.Clients;
+public class MusementApiClientTests
+{
+    private readonly HttpClientFactory _httpClientFactory = new("/api/v3/cities");
+
+    [Fact]
+    public async Task GetCitiesAsync_WhenOk_ReturnsExpectedCollection()
+    {
+        // Arrange
+        _httpClientFactory.HttpStatusCode = HttpStatusCode.OK;
+
+        List<City> expectedCities = new()
+        {
+            new("Milan", 45.464664m, 9.188540m),
+            new("Rome", 41.902782m, 12.496366m),
+            new("Rabat", 34.01325m, -6.83255m)
+        };
+        _httpClientFactory.Content = expectedCities;
+
+        HttpClient httpClient = _httpClientFactory.CreateClient();
+
+        MusementApiClient musementApiClient = new(httpClient);
+
+        // Act
+        ICollection<City> actualCities = await musementApiClient.GetCitiesAsync();
+
+        // Assert
+        actualCities.Should().BeEquivalentTo(expectedCities);
+    }
+}
---
+++ WeatherForecast.Cli/Clients/MusementApiClient.cs
@@ -0,0 +1,22 @@
+﻿using System.Net.Http.Json;
+using WeatherForecast.Cli.Interfaces;
+using WeatherForecast.Cli.Models;
+
+namespace WeatherForecast.Cli.Clients;
+internal sealed class MusementApiClient : IMusementApiClient
+{
+    private readonly HttpClient _httpClient;
+
+    public MusementApiClient(HttpClient httpClient)
+    {
+        _httpClient = httpClient;
+    }
+
+    public async Task<ICollection<City>> GetCitiesAsync()
+    {
+        List<City>? cities = await _httpClient.GetFromJsonAsync<List<City>>(
+            "/api/v3/cities").ConfigureAwait(false);
+
+        return cities ?? new List<City>();
+    }
+}
