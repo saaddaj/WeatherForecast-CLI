2f000a74921c05a8e2beefde14fbb4baff742406
Samy A <sam_dd@hotmail.fr>
2022-10-02 18:23:24 +0100

    First Implementation of the IWeatherApiClient interface

---
+++ WeatherForecast.Cli.Tests/Clients/WeatherApiClientTests.cs
@@ -0,0 +1,47 @@
+﻿using System.Globalization;
+using System.Net;
+using WeatherForecast.Cli.Clients;
+using WeatherForecast.Cli.Models;
+
+namespace WeatherForecast.Cli.Tests.Clients;
+public class WeatherApiClientTests
+{
+    private const string _apiKey = "testApiKey";
+
+    private const decimal _latitude = 45.464664m;
+
+    private const decimal _longitude = 9.188540m;
+
+    private readonly HttpClientFactory _httpClientFactory;
+
+    public WeatherApiClientTests()
+    {
+        string request = string.Format("/v1/forecast.json?key={0}&q={1},{2}&days=2",
+            _apiKey,
+            _latitude.ToString(CultureInfo.InvariantCulture),
+            _longitude.ToString(CultureInfo.InvariantCulture));
+
+        _httpClientFactory = new HttpClientFactory(request);
+    }
+
+    [Fact]
+    public async Task GetForecastByCoordinatesAsync_WhenOk_ReturnsExpectedForecast()
+    {
+        // Arrange
+        _httpClientFactory.HttpStatusCode = HttpStatusCode.OK;
+
+        Forecast expectedForecast = new("Sunny", "Partly cloudy");
+        _httpClientFactory.Content = expectedForecast;
+
+        WeatherApiClient weatherApiClient = new(_httpClientFactory.CreateClient(), _apiKey);
+
+        // Act
+        Forecast? actualForecast = await weatherApiClient.GetNext2DaysForecastByCoordinatesAsync(
+            _latitude,
+            _longitude);
+
+        // Assert
+        actualForecast.Should().NotBeNull();
+        actualForecast.Should().BeEquivalentTo(expectedForecast);
+    }
+}
---
+++ WeatherForecast.Cli/Clients/WeatherApiClient.cs
@@ -0,0 +1,28 @@
+﻿using System.Globalization;
+using System.Net.Http.Json;
+using WeatherForecast.Cli.Interfaces;
+using WeatherForecast.Cli.Models;
+
+namespace WeatherForecast.Cli.Clients;
+internal sealed class WeatherApiClient : IWeatherApiClient
+{
+    private readonly HttpClient _httpClient;
+
+    private readonly string _apiKey;
+
+    public WeatherApiClient(HttpClient httpClient, string apiKey)
+    {
+        _httpClient = httpClient;
+        _apiKey = apiKey;
+    }
+
+    public async Task<Forecast?> GetNext2DaysForecastByCoordinatesAsync(decimal latitude, decimal longitude)
+    {
+        string endpoint = string.Format("/v1/forecast.json?key={0}&q={1},{2}&days=2",
+            _apiKey,
+            latitude.ToString(CultureInfo.InvariantCulture),
+            longitude.ToString(CultureInfo.InvariantCulture));
+
+        return await _httpClient.GetFromJsonAsync<Forecast>(endpoint).ConfigureAwait(false);
+    }
+}
